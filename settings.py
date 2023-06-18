9b89667db8b49442c186ebfa3805db28c5a215e4
AxDjuraev <axdjuraev@gmail.com>
2023-06-18 19:25:22 +0500

    feat: settings

---
+++ settings.py
@@ -0,0 +1,22 @@
+from pydantic import BaseSettings
+from sqlalchemy.engine.url import URL
+
+
+class DBSettings(BaseSettings):
+    DB_DRIVERNAME: str = "postgresql+asyncpg"
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_USERNAME: str
+    DB_PASSWORD: str
+    DB_DATABASE: str
+
+    @property
+    def db_connection_string(self):
+        return URL.create(
+            drivername=self.DB_DRIVERNAME,
+            username=self.DB_USERNAME,
+            password=self.DB_PASSWORD,
+            host=self.DB_HOST,
+            port=self.DB_PORT,
+            database=self.DB_DATABASE,
+        )
