dd01c339fe18d842b8d16fa97e6e7adee731f4d7
axdjuraev <axdjuraev@gmail.com>
2023-08-22 14:17:29 +0500

    feat: base schemas

---
+++ axsqlalchemy/schema.py
@@ -0,0 +1,21 @@
+from uuid import UUID
+from datetime import datetime
+from pydantic import BaseModel as _BaseModel
+
+class BaseModelAt(_BaseModel):
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
+
+
+class BaseModel(BaseModelAt):
+    class Config:
+        orm_mode = True
+
+
+class BaseModelUUID(BaseModel):
+    id: UUID
+
+
+class BaseModelInt(BaseModel):
+    id: int
+
