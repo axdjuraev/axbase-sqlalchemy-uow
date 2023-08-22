bc2ae053d34a84ab7cacd070e72cc051d185674a
axdjuraev <axdjuraev@gmail.com>
2023-08-22 11:08:23 +0500

    feat (testsing:__init__): add with_session

--- axsqlalchemy/testing/__init__.py
+++ axsqlalchemy/testing/__init__.py
@@ -1,5 +1,6 @@
-from .base_async_repo_test import BaseAsyncRepoTest
+from .base_async_repo_test import BaseAsyncRepoTest, with_session
 
 __all__ = [
     'BaseAsyncRepoTest',
+    'with_session',
 ]
