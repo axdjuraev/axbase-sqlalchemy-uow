5250c0f12d5c64427de23f226f0ce34e2e7e6a95
axdjuraev <axdjuraev@gmail.com>
2023-08-24 13:33:10 +0500

    feat (schemas): ignore_deactive_schema

--- axsqlalchemy/schema.py
+++ axsqlalchemy/schema.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 from datetime import datetime
-from pydantic import BaseModel as _BaseModel
+from pydantic import BaseModel as _BaseModel, root_validator
 
 class BaseModelAt(_BaseModel):
     created_at: datetime | None = None
@@ -18,4 +18,15 @@ class BaseModelUUID(BaseModel):
 
 class BaseModelInt(BaseModel):
     id: int | None = None
+
+
+class IgnoreDeactiveSchema(BaseModel): 
+    @root_validator(pre=True)
+    def validate_value(cls, values):
+        if 'is_active' in values and not values['is_active']:
+            raise ValueError        
+        return values
+    
+    class Config:
+        skip_invalid_collections_items = True 
 
