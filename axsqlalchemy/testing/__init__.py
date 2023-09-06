121de016460130122db44f517104f9b409d693a0
axdjuraev <axdjuraev@gmail.com>
2023-09-06 18:18:57 +0500

    update (base_test): add sync create all models

--- axsqlalchemy/testing/__init__.py
+++ axsqlalchemy/testing/__init__.py
@@ -1,6 +1,12 @@
-from .base_async_repo_test import BaseAsyncRepoTest, with_session
+from .base_async_repo_test import (
+    BaseAsyncRepoTest, 
+    with_session,
+    with_repo,
+)
+
 
 __all__ = [
     'BaseAsyncRepoTest',
     'with_session',
+    'with_repo',
 ]
