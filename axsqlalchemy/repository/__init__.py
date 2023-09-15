79a6603c9da09d3d9dd2283d3bb0fbea3b51def1
axdjuraev <axdjuraev@gmail.com>
2023-09-15 10:26:31 +0500

    rm: repo_hell

---
+++ axsqlalchemy/repository/__init__.py
@@ -0,0 +1,19 @@
+from .adder import AdderRepo
+from .getter import GetterRepo
+from .updateter import UpdateterRepo
+from .status_updater import StatusUpdaterRepo
+from .deleter import DeleterRepo
+from .paginated_all_getter import PaginatedAllGetterRepo
+from .common import BaseRepository
+
+
+__all__ = [
+	'AdderRepo',
+	'GetterRepo',
+	'UpdateterRepo',
+	'StatusUpdaterRepo',
+	'DeleterRepo',
+	'PaginatedAllGetterRepo',
+	'BaseRepository',
+]
+
---
+++ axsqlalchemy/repository/common.py
@@ -0,0 +1,24 @@
+from typing import Generic
+
+from .types import TIModel, TOModel, TDBModel
+from .adder import AdderRepo
+from .getter import GetterRepo
+from .updateter import UpdateterRepo
+from .status_updater import StatusUpdaterRepo
+from .deleter import DeleterRepo
+from .paginated_all_getter import PaginatedAllGetterRepo
+
+
+class BaseRepository(
+	AdderRepo[TDBModel, TIModel, TOModel],
+	GetterRepo[TDBModel, TIModel, TOModel],
+	UpdateterRepo[TDBModel, TIModel, TOModel],
+	StatusUpdaterRepo[TDBModel, TIModel, TOModel],
+	DeleterRepo[TDBModel, TIModel, TOModel],
+	PaginatedAllGetterRepo[TDBModel, TIModel, TOModel],
+    Generic[TDBModel, TIModel, TOModel],
+):
+    def __init_subclass__(cls) -> None:
+        types = getattr(cls, "__orig_bases__")[0].__args__
+        cls.Model, cls.Schema, cls.OSchema = types
+
