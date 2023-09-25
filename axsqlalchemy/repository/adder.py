a00b5efff1b7be099e40df63621bf563533e90df
axdjuraev <axdjuraev@gmail.com>
2023-09-25 07:52:56 +0500

    ref (AdderRepo): autocommit -> autosave

--- axsqlalchemy/repository/adder.py
+++ axsqlalchemy/repository/adder.py
@@ -5,14 +5,14 @@ from .base import BaseRepoCreator
 
 
 class AdderRepo(BaseRepoCreator[TDBModel, TIModel, TOModel], Generic[TDBModel, TIModel, TOModel]):
-    async def add(self, obj: TIModel, autocommit=True) -> TOModel:
+    async def add(self, obj: TIModel, autosave=True) -> TOModel:
         if type(obj) in (self.OSchema, self.Schema):
             obj = self.Schema.from_orm(obj)
 
         mobj = self.Model(**obj.dict())
         self.session.add(mobj)
         
-        if autocommit:
+        if autosave:
             await self.session.commit()
 
         return self.OSchema.from_orm(mobj)
