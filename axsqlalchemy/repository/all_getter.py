b27d6ca980f8da805b0917b5d7ae39061d7e6769
axdjuraev <axdjuraev@gmail.com>
2023-10-30 08:30:22 +0500

    ref (AllGetterRepo): add __base_all_query

--- axsqlalchemy/repository/all_getter.py
+++ axsqlalchemy/repository/all_getter.py
@@ -1,20 +1,23 @@
-from typing import Generic, Iterable, List
+from typing import Generic, Iterable, List, Optional
 from sqlalchemy import select
 from pydantic import parse_obj_as
 
 from .types import TIModel, TOModel, TDBModel
 from .base import BaseRepoCreator
 
-
 class AllGetterRepo(BaseRepoCreator[TDBModel, TIModel, TOModel], Generic[TDBModel, TIModel, TOModel]):
     ids4all: Iterable = tuple()
 
-    async def all(self, *ids, filters: Iterable = tuple()) -> list[TOModel]:
+    @property
+    def _base_all_query(self): 
+        return select(self.Model)
+
+    async def all(self, *ids, filters: Iterable = tuple(), query = None) -> list[TOModel]:
         filters = self._get_filters(ids, columns=self.ids4all, extra_filters=filters)
 
         objs = (
             await self.session.execute(
-                select(self.Model)
+                (query or self._base_all_query)
                 .where(*filters)
                 .order_by(self.Model.created_at.desc()) 
             )
