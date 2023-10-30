baecfe208f741ca77e744bb7ba40ddb39d2b7965
axdjuraev <axdjuraev@gmail.com>
2023-10-30 09:04:35 +0500

    feat: base get query

--- axsqlalchemy/repository/getter.py
+++ axsqlalchemy/repository/getter.py
@@ -6,13 +6,18 @@ from .base import BaseRepoCreator
 
 
 class GetterRepo(BaseRepoCreator[TDBModel, TIModel, TOModel], Generic[TDBModel, TIModel, TOModel]):
-    async def get(self, *ids, filters: Iterable = tuple()) -> Optional[TOModel]:
+    @property
+    def _base_get_query(self):
+        return select(self.Model)
+
+    async def get(self, *ids, filters: Iterable = tuple(), query = None) -> Optional[TOModel]:
+        query = query if query is not None else self._base_get_query
         filters = self._get_filters(ids, extra_filters=filters, use_defaults=False)
 
         obj = (
             (
                 await self.session.execute(
-                    select(self.Model)
+                    query
                     .where(and_(*filters))
                     .order_by(self.Model.created_at.desc()),
                 )
