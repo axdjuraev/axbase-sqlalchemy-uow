6c089766e7e04ff666379819b704d59a78292a38
axdjuraev <axdjuraev@gmail.com>
2023-12-15 08:56:24 +0500

    ref (BaseRepoCreator): add session type annotation

--- axsqlalchemy/repository/base.py
+++ axsqlalchemy/repository/base.py
@@ -14,6 +14,7 @@ class BaseRepoCreator(AbstractAsyncRepository, Generic[TDBModel, TIModel, TOMode
 
     _default_filters: tuple = tuple()
     ids: tuple[Column]
+    session: AsyncSession
 
     def __init__(self, session: AsyncSession) -> None:
         super().__init__(session)
