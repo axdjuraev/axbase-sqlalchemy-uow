494e297fe13dfa6710ae64dc1b9dc2090c77aadf
axdjuraev <axdjuraev@gmail.com>
2024-02-22 15:57:51 +0500

    ref: __init__

--- axsqlalchemy/__init__.py
+++ axsqlalchemy/__init__.py
@@ -1,13 +1,11 @@
 from . import utils
 from .model import AbstractBaseModel, BaseTableAt
 from .repository import BaseRepository
-from .uow import UOW, UOWFactory
+
 
 __all__ = [
     "AbstractBaseModel",
     "BaseTableAt",
     "BaseRepository",
-    "UOWFactory",
-    "UOW",
     "utils",
 ]
