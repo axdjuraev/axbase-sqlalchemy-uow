a1fa89705eb61a8defe074b77edd209406d1a31c
axdjuraev <axdjuraev@gmail.com>
2024-02-22 15:51:51 +0500

    feat (utils:creation): get_uowf

--- axsqlalchemy/utils/creation.py
+++ axsqlalchemy/utils/creation.py
@@ -1,5 +1,22 @@
-from sqlalchemy.ext.asyncio import AsyncEngine
+from typing import TYPE_CHECKING, TypeVar
+from axabc.db.repo_collector import BaseRepoCollector
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 from sqlalchemy.orm import decl_base
+
+
+if TYPE_CHECKING:
+    from axsqlalchemy.settings import Settings as DBSettings
+    from axabc.db import AsyncUOWFactory
+    
+
+TRepoCollection = TypeVar("TRepoCollection", bound=BaseRepoCollector)
+
+
+def get_uowf(settings: "DBSettings", TypeRepoCollection: type[TRepoCollection]) -> "AsyncUOWFactory[TRepoCollection]":
+    engine = create_async_engine(settings.db_connection_string)
+    session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)  # type: ignore
+    session_maker.engine = engine  # type: ignore
+    return AsyncUOWFactory(TypeRepoCollection, session_maker)
 
 
 async def create_models(engine: AsyncEngine, base: decl_base):
